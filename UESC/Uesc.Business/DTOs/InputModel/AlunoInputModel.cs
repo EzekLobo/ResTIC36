4d4a7c4e0129cfbc9062f60695a58c6e2cc9138e
Ezequiel Lobo <eloliveira.cic@uesc.br>
2025-03-06 11:29:33 -0300

    Add validation attributes

--- UESC/Uesc.Business/DTOs/InputModel/AlunoInputModel.cs
+++ UESC/Uesc.Business/DTOs/InputModel/AlunoInputModel.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 namespace Uesc.Api.DTOs.InputModel;
 
 public class AlunoInputModel
 {
+    [Required(ErrorMessage = "O campo matrícula é obrigatório.")]
+    [Range(1, int.MaxValue, ErrorMessage = "A matrícula deve ser um número positivo.")]
     public int Matricula { get; set; }
+
+    [StringLength(50, ErrorMessage = "O nome deve ter no máximo 50 caracteres.")]
     public string Nome{ get; set; }
 }
